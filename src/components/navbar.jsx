2e5679b6713c8d24afe7e904e330a0cf4e615d2a
Ivan Tarozzi <itarozzi@gmail.com>
2020-05-21 10:59:09 +0200

    metodo alternativo per definire i componenti, non usando classi ma funzioni

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 
-class NavBar extends Component {
+
+// Stateless Functional Component
+const NavBar = (props) => {
     
-    render() { 
         return (<nav className="navbar navbar-light bg-light">
             <a className="navbar-brand" href="#">
                 Navbar 
-                <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+                <span className="badge badge-pill badge-secondary">{props.totalCounters}</span>
             </a>
         </nav>  );
-    }
-}
+
+};  
+
+// class NavBar extends Component {
+    
+//     render() { 
+//         return (<nav className="navbar navbar-light bg-light">
+//             <a className="navbar-brand" href="#">
+//                 Navbar 
+//                 <span className="badge badge-pill badge-secondary">{this.props.totalCounters}</span>
+//             </a>
+//         </nav>  );
+//     }
+// }
  
 export default NavBar
 ;
